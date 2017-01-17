6720d17364b13296083dc56626cd5be0a5993eba
Anwesh43 <anweshthecool0@gmail.com>
2017-01-17 18:01:37 +0530

    Adding nodejs files

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+app.get('/hello',function(req,res){
+    res.send('hello world')
+})
+app.listen(8080,function(){
+    console.log("listening on port 8000")
+})
